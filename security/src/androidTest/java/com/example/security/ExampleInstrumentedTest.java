1daebe99917f0c37e2392e998b55966bcfa631f8
Iosu <iosu@safeboda.com>
2018-12-05 13:33:23 +0100

    remove copyright

--- security/src/androidTest/java/com/example/security/ExampleInstrumentedTest.java
+++ security/src/androidTest/java/com/example/security/ExampleInstrumentedTest.java
@@ -1,25 +1,10 @@
-/*
- * Copyright (C) 2018 SEAT, S.A - All Rights Reserved
- *
- * This file is part of MySeat App.
- *
- * Unauthorized reproduction, copying, or modification, of this file, via
- * any medium is strictly prohibited.
- *
- * This code is Proprietary and Confidential.
- *
- * All the 3rd parties libraries included in the project are regulated by
- * their own licenses.
- */
-
 package com.example.security;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -31,7 +16,7 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
+    public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
--- security/src/main/java/com/example/security/CryptoUtils.java
+++ security/src/main/java/com/example/security/CryptoUtils.java
@@ -1,17 +1,3 @@
-/*
- * Copyright (C) 2018 SEAT, S.A - All Rights Reserved
- *
- * This file is part of MySeat App.
- *
- * Unauthorized reproduction, copying, or modification, of this file, via
- * any medium is strictly prohibited.
- *
- * This code is Proprietary and Confidential.
- *
- * All the 3rd parties libraries included in the project are regulated by
- * their own licenses.
- */
-
 package com.example.security;
 
 import android.util.Base64;
